47888e0a939d286547a397a905f98dc8353bbc32
Ryan Smith <ryanmsmith251@gmail.com>
2021-12-02 16:02:58 +0000

    day 2

--- aoc.go
+++ aoc.go
@@ -5,11 +5,12 @@ import (
 	"os"
 	"log"
 	"bufio"
+	"strings"
 	"strconv"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("input2.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,22 +20,32 @@ func main() {
 		}
 	}()
 	
-	count := 0
-	prev := 0
+	depth := 0
+	horizontal := 0
+	aim := 0
 	scanner := bufio.NewScanner(file)
 	// i := 0
 	for scanner.Scan() {
 		current := scanner.Text()
-		cur, _ := strconv.Atoi(current)
-		// fmt.Println(cur, count, prev)
-		if cur > prev && prev != 0 {
-			count++
+		line := strings.Split(current, " ")
+		change, _ := strconv.Atoi(line[1])
+		if line[0] == "forward" {
+			if aim != 0 {
+				depth += (aim * change)
+			}
+			horizontal += change
+		} else if line[0] == "down" {
+			aim += change
+		} else if line[0] == "up" {
+			aim -= change
+		} else{
+			fmt.Println("Error")
 		}
-		prev = cur
+		// fmt.Println("depth:", depth, "horizontal:", horizontal, "aim:", aim, "depth*horiz:", depth * horizontal)
 		// i++
 		// if i > 5 {
 		// 	os.Exit(1)
 		// }
 	}
-	fmt.Println(count)
+	fmt.Println(depth, horizontal, aim, depth * horizontal)
 }
